01f56b4256326e8172ea582e700ebae490b370e2
Will Royal <william.j.royal@gmail.com>
2025-01-26 14:42:10 -0800

    assignment 5

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -15,6 +15,7 @@ namespace Testing
         public IEnumerable<Category> GetCategories();
 
         public Product AssignCategory();
+        public void DeleteProduct(Product product);
 
     }
 }
